ffba7a0d8861c63d692597189ac74cffbfd585ca
Md. Zohir Raihan <zohir@erainfotechbd.com>
2023-07-14 00:10:44 +0600

    Add validation in find customer

--- frm-meveo/meveo-web/src/main/java/com/poortoys/dtos/CustomerDTO.java
+++ frm-meveo/meveo-web/src/main/java/com/poortoys/dtos/CustomerDTO.java
@@ -2,25 +2,32 @@ package com.poortoys.dtos;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.RequestScoped;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by Md. Zohir Raihan on 12/7/2023.
  *
  * Interface that used only for naming convention for methods for services
  */
 
+@RequestScoped
 public class CustomerDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private long customerId;
+	@NotNull(message = "customerId is required")
+	private Long customerId;
 	
+	@NotEmpty(message = "customerName is required")
 	private String customerName;
 
-	public long getCustomerId() {
+	public Long getCustomerId() {
 		return customerId;
 	}
 
-	public void setCustomerId(long customerId) {
+	public void setCustomerId(Long customerId) {
 		this.customerId = customerId;
 	}
 
--- frm-meveo/meveo-web/src/main/java/com/poortoys/resources/CustomerResource.java
+++ frm-meveo/meveo-web/src/main/java/com/poortoys/resources/CustomerResource.java
@@ -1,15 +1,23 @@
 package com.poortoys.resources;
 
+import javax.enterprise.context.RequestScoped;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.poortoys.dtos.CustomerDTO;
+import com.poortoys.exceptions.ApplicationException;
 
 /**
  * Created by Md. Zohir Raihan on 12/7/2023.
@@ -18,24 +26,28 @@ import com.poortoys.dtos.CustomerDTO;
  */
 
 @Path("/v1/customers")
+@RequestScoped
 public class CustomerResource {
 	
 	@Path("/create")
 	@POST
     @Consumes(value = MediaType.APPLICATION_JSON)
-    @Produces(value = MediaType.APPLICATION_JSON)
-    public Response create(CustomerDTO customerDTO) {
+    @Produces(value = MediaType.APPLICATION_JSON)	
+    public Response create(@Valid CustomerDTO customerDTO) {
     	    	    	
         return Response.status(200).entity(customerDTO).build();
     }
 	
 	@GET
-    @Path("{cusomerId}")
+    //@Path("{cusomerId}")
     @Produces(value = MediaType.APPLICATION_JSON)
-    public Response find(@PathParam("cusomerId") int cusomerId) {
+    public Response find(@QueryParam("cusomerId") Long cusomerId) {
 		
-		if((Integer) cusomerId == null) {
-			return Response.status(400).entity("cusomerId missing in API URL").build();
+		if((Long) cusomerId == null) {
+			throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity("cusomerId parameter value is mandatory").build());
+			//throw new NotFoundException("cusomerId not found in URL path");
+			//throw new ApplicationException(Response.Status.NOT_FOUND);
+			//return Response.status(400).entity("cusomerId missing in API URL").build();
 		}
 		
 		CustomerDTO customerDTO = new CustomerDTO();
